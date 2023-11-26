f1353ec4b2683215661e300c5d823a075389bce2
Christa Lococo <montecitocandleco@gmail.com>
2023-11-26 13:36:31 -0700

    improved product views

--- montecito_candle_company/store/urls.py
+++ montecito_candle_company/store/urls.py
@@ -1,8 +1,11 @@
 # store/urls.py
 from django.urls import path
-from .views import product_list
+from .views import product_list, add_product, product_detail, category_view
 
 urlpatterns = [
     path('products/', product_list, name='product_list'),
+    path('products/<int:product_id>/', product_detail, name='product_detail'),
+    path('categories/<str:category>/', category_view, name='category_view'),
+    path('add/', add_product, name='add_product'),
     # Add other URL patterns as needed
 ]
--- montecito_candle_company/store/views.py
+++ montecito_candle_company/store/views.py
@@ -1,11 +1,8 @@
 # montecito_candle_company/store/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Product
 from .forms import ProductForm
-
-def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'store/product_list.html', {'products': products})
 
 def add_product(request):
     if request.method == 'POST':
@@ -17,3 +14,27 @@ def add_product(request):
         form = ProductForm()
 
     return render(request, 'store/add_product.html', {'form': form})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'store/product_detail.html', {'product': product})
+
+def category_view(request, category):
+    products = Product.objects.filter(category=category)
+    return render(request, 'store/category.html', {'products': products, 'category': category})
+
+def product_list(request):
+    products = Product.objects.all()
+
+    # Pagination
+    page = request.GET.get('page', 1)
+    paginator = Paginator(products, 10)  # Show 10 products per page
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    return render(request, 'store/product_list.html', {'products': products})
